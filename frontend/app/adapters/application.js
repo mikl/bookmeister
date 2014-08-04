8d2e69f0846cfbfd52e4c4871ddf77778be96fd4
Mikkel Hoegh <mikkel@hoegh.org>
2014-08-04 21:51:23 +0200

    Basic bookmark list based on fixtures.

---
+++ frontend/app/adapters/application.js
@@ -0,0 +1,4 @@
+import DS from 'ember-data';
+
+// FIXME: Use the fixture adapter until we get a proper adapter done.
+export default DS.FixtureAdapter.extend({});
---
+++ frontend/app/models/bookmark.js
@@ -0,0 +1,15 @@
+import DS from 'ember-data';
+
+var Bookmark = DS.Model.extend({
+  url: DS.attr('string'),
+});
+
+Bookmark.reopenClass({ 
+  FIXTURES: [
+    { id: 1, url: 'http://mikkel.hoegh.org/' },
+    { id: 2, url: 'http://hoegh.io/' },
+    { id: 3, url: 'http://rhabarberbarbarabarbarbarenbartbarbierbierbar.com/' }
+  ]
+});
+
+export default Bookmark;
