a591b1c450a7ed49c984e10dcde116d0b7af2ef1
Mikkel Hoegh <mikkel@hoegh.org>
2014-08-17 00:16:06 +0200

    Added cancel button to edit form.

--- frontend/app/controllers/bookmarks/edit.js
+++ frontend/app/controllers/bookmarks/edit.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
   actions: {
+    cancel: function () {
+      this.get('model').rollback();
+
+      this.transitionToRoute('bookmarks');
+    },
+
     saveBookmark: function () {
       this.get('model').save();
 
