38047f6b2e55cfb1c3ca07c940bc8df4d54a0d6a
Mikkel Hoegh <mikkel@hoegh.org>
2014-10-01 01:04:25 +0200

    Stop initial fetch of CSRF token.

--- frontend/app/services/drupal-auth.js
+++ frontend/app/services/drupal-auth.js
@@ -1,16 +1,9 @@
-"use strict";
 import Ember from 'ember';
 import config from '../config/environment';
 
 var csrfTokenRequest;
 
 export default Ember.Object.extend({
-  init: function() {
-    // Since the CSRF token is used for all other backend requests, we might as
-    // well fetch it immediately when the service is loaded.
-    this.getCsrfToken();
-  },
-
   getCsrfToken: function () {
     // If the CSRF request was not already created, create it.
     if (!csrfTokenRequest) {
@@ -18,12 +11,14 @@ export default Ember.Object.extend({
         contentType: 'application/json',
         dataType: 'json',
         type: 'POST',
-        url: config.bookmeisterServer + 'api/bookmeister/user/token.json'
+        url: config.bookmeisterServer + '/api/bookmeister/user/token.json'
       });
     }
 
     // Return the promise so it can be used by the caller.
     return csrfTokenRequest;
   }
+
+
 });
 
