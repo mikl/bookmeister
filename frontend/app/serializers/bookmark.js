7b5bbd33ba3776ddce19ef0beb57d08195cdc220
Mikkel Hoegh <mikkel@hoegh.org>
2014-10-01 13:23:32 +0200

    Avoid sending NULL values back to the server.

--- frontend/app/serializers/bookmark.js
+++ frontend/app/serializers/bookmark.js
@@ -12,5 +12,18 @@ export default DS.JSONSerializer.extend({
   // Decamelize and lowercase keys sent to the server.
   keyForAttribute: function (attr) {
     return Ember.String.underscore(attr);
+  },
+
+  serialize: function(record, options) {
+    var json = this._super.apply(this, arguments);
+
+    Object.keys(json).forEach(function (key) {
+      // Avoid sending NULL values back to the server.
+      if (json[key] === null) {
+        delete json[key];
+      }
+    });
+
+    return json;
   }
 });
