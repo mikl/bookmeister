99302870ad16c852b3ea42c5f75a5cc4d394a12a
Mikkel Hoegh <mikkel@hoegh.org>
2014-10-01 13:14:02 +0200

    Fixed URL building for RestWS.

--- frontend/app/adapters/bookmark.js
+++ frontend/app/adapters/bookmark.js
@@ -3,7 +3,24 @@ import ApplicationAdapter from './application';
 import Ember from 'ember';
 
 export default ApplicationAdapter.extend({
-  pathForType: function (type) {
-    return 'bookmeister_bookmark.json';
+  buildURL: function (type, id, record) {
+    var url = [],
+      host = Ember.get(this, 'host'),
+      prefix = this.urlPrefix();
+
+    if (Ember.$.isNumeric(id)) {
+      url.push('bookmeister_bookmark/' + parseInt(id, 10) + '.json');
+    }
+    else {
+      url.push('bookmeister_bookmark.json');
+    }
+
+
+    if (prefix) { url.unshift(prefix); }
+
+    url = url.join('/');
+    if (!host && url) { url = '/' + url; }
+
+    return url;
   }
 });
