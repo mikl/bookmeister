f45c032072c8abe9b7b0d496a222c38cc402a664
Mikkel Hoegh <mikkel@hoegh.org>
2014-10-25 13:55:05 +0200

    Redirect after bookmark save completes.

--- frontend/app/controllers/bookmarks.js
+++ frontend/app/controllers/bookmarks.js
@@ -6,18 +6,21 @@ export default Ember.Controller.extend({
 
   actions: {
     saveNewBookmark: function () {
+      var controller = this;
       var bookmark;
       var url = this.get('newBookmarkURL', '');
 
       if (url.length > 3) {
         bookmark = this.store.createRecord('bookmark');
         bookmark.set('url', url);
-        bookmark.save();
+
+        // Save, and when that succeeds, proceed to edit the bookmark.
+        bookmark.save().then(function (newBm) {
+          controller.transitionToRoute('bookmarks.edit', newBm.get('id'));
+        });
 
         // Reset the input field to prevent double posts.
         this.set('newBookmarkURL', '');
-
-        this.transitionToRoute('bookmarks.edit', bookmark.get('id'));
       }
     }
   }
