525d96ded03cb7674b90af26a5d709bc5dbb0807
Mikkel Hoegh <mikkel@hoegh.org>
2015-04-15 22:15:46 +0200

    Use index template for the front page.

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function() {
-  this.resource('bookmarks', {path: "/"}, function() {
+  this.resource('bookmarks', {path: "/bookmark"}, function() {
     this.route('edit', {path: "/bookmark/:bookmark_id"});
     this.route('delete', {path: "/bookmark/:bookmark_id/delete"});
   });
