a02fffd126fc5877e73387334797754dd5cc084a
Mikkel Hoegh <mikkel@hoegh.org>
2015-04-15 18:27:13 +0200

    New router file syntax.

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -5,11 +5,9 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {
+export default Router.map(function() {
   this.resource('bookmarks', {path: "/"}, function() {
     this.route('edit', {path: "/bookmark/:bookmark_id"});
     this.route('delete', {path: "/bookmark/:bookmark_id/delete"});
   });
 });
-
-export default Router;
