03c67958ee354dd55c5b283c9747b1a832eff2e2
Mikkel Hoegh <mikkel@hoegh.org>
2015-05-29 14:28:33 +0200

    Use standard URL patterns for bookmarks.

--- webapp/app/adapters/bookmark.js
+++ webapp/app/adapters/bookmark.js
@@ -1,26 +1,4 @@
-"use strict";
 import ApplicationAdapter from './application';
-import Ember from 'ember';
 
 export default ApplicationAdapter.extend({
-  buildURL: function (type, id, record) {
-    var url = [],
-      host = Ember.get(this, 'host'),
-      prefix = this.urlPrefix();
-
-    if (Ember.$.isNumeric(id)) {
-      url.push('bookmeister_bookmark/' + parseInt(id, 10));
-    }
-    else {
-      url.push('bookmeister_bookmark');
-    }
-
-
-    if (prefix) { url.unshift(prefix); }
-
-    url = url.join('/');
-    if (!host && url) { url = '/' + url; }
-
-    return url;
-  }
 });
