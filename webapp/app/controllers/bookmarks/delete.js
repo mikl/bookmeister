feb9d6c289c58f5b183c5014277d6d97b29ba3bc
Mikkel Hoegh <mikkel@hoegh.org>
2015-06-14 00:19:20 +0200

    No longer using deprecated ObjectController.

--- webapp/app/controllers/bookmarks/delete.js
+++ webapp/app/controllers/bookmarks/delete.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
   actions: {
     cancel: function () {
       this.transitionToRoute('bookmarks');
