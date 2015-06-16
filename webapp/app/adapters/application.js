7691f34c8f53d7cce382ed84de8d2d9d94837efa
Mikkel Hoegh <mikkel@hoegh.org>
2015-06-16 21:27:48 +0200

    Generate a random UUID for new models.

--- webapp/app/adapters/application.js
+++ webapp/app/adapters/application.js
@@ -2,5 +2,24 @@ import DS from 'ember-data';
 import config from '../config/environment';
 
 export default DS.RESTAdapter.extend({
-  host: config.bookmeisterServer
+  host: config.bookmeisterServer,
+
+  /**
+   * Generate a random UUID for new models.
+   */
+  generateIdForRecord: function () {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (char) {
+      var randVal = Math.random() * 16 | 0;
+
+      var hexdigit;
+
+      if (char == 'x') {
+        hexdigit = randVal;
+      } else {
+        hexdigit = (randVal & 0x3 | 0x8);
+      }
+
+      return hexdigit.toString(16);
+    });
+  },
 });
